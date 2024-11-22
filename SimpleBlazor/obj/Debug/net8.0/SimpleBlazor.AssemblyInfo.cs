8821b6fe86c07a1369abee48044268c5c5a1aa13
Opitz <opitz@Opitzs-MacBook-Air.local>
2024-11-21 23:30:12 -0700

    Fixed to two working tabs

--- SimpleBlazor/obj/Debug/net8.0/SimpleBlazor.AssemblyInfo.cs
+++ SimpleBlazor/obj/Debug/net8.0/SimpleBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f09e5aed3783c35e1d88aa3382185a3caec3cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da0495fb4bb58924a96dcd47536be73f35f7f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SimpleLogic/obj/Debug/net8.0/SimpleLogic.AssemblyInfo.cs
+++ SimpleLogic/obj/Debug/net8.0/SimpleLogic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32003c3a796f11b6a597698854c4098d7b7c27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da0495fb4bb58924a96dcd47536be73f35f7f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
